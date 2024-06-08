b62796291a41e026e0ac023b1b02419724d5815c
Safizapi <velikayasoplya@gmail.com>
2024-06-08 21:18:15 +0300

    Added docs

--- bonkBot/Room.py
+++ bonkBot/Room.py
@@ -1,10 +1,24 @@
 import socketio
 
 from .Game import Game
-from .GameTypes import Modes
+from .Types import Modes
 
 
 class Room:
+    """
+    Class for rooms from bonk.io room list
+
+    :param bot: bot class that loaded the room
+    :param room_id: database ID of room
+    :param name: name of the room
+    :param players: current amount of players that play in this room
+    :param max_players: maximal amount of players that can play in this room at the same time
+    :param has_password: whether room has password or not
+    :param mode: the mode that is currently played in the room
+    :param min_level: the minimal level that is required to join the room
+    :param max_level: the maximal level along with you can join the room
+    """
+
     def __init__(
         self,
         bot,
@@ -27,14 +41,35 @@ class Room:
         self.min_level: int = min_level
         self.max_level: int = max_level
 
-    def join(self, password="") -> Game:
-        return Game(
+    async def join(self, password="") -> Game:
+        """
+        Joins game from room list
+
+        :param password: password to join room
+
+        Example usage::
+
+            bot = bonk_account_login("name", "pass")
+
+            async def main():
+                room = [room for room in bot.get_rooms() if room.name == "test room"][0]
+                game = await room.join()
+
+                await bot.run()
+
+            asyncio.run(main())
+        """
+
+        game = Game(
             self.bot,
             self.name,
-            socketio.AsyncClient(ssl_verify=False, logger=True, engineio_logger=True),
+            socketio.AsyncClient(ssl_verify=False),
             False,
             self.mode,
             False,
             self.bot.event_emitter,
             game_join_params=[self.room_id, password]
         )
+        await game.connect()
+
+        return game

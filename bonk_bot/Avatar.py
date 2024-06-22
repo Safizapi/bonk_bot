128145a17991601b4779bcfd8d539c40e7070d48
Safizapi <147392459+Safizapi@users.noreply.github.com>
2024-06-22 19:22:26 +0300

    Update Avatar.py

--- bonk_bot/Avatar.py
+++ bonk_bot/Avatar.py
@@ -1,7 +1,3 @@
 class Avatar:
-    def __init__(self, bot, json_data: dict):
-        self.bot = bot
+    def __init__(self, json_data: dict):
         self.json_data: dict = json_data
-
-    def set_as_bot_avatar(self):
-        self.bot.main_avatar = Avatar(self.bot, self.json_data)

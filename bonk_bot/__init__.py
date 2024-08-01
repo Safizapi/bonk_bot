48aa00a827653984ce716c5582bb0562b1c750af
Safizapi <147392459+Safizapi@users.noreply.github.com>
2024-08-02 00:47:21 +0300

    Create __init__.py

---
+++ bonk_bot/__init__.py
@@ -0,0 +1,19 @@
+from .bot import *
+from .bonk_online import BonkOnline
+from .parsers import *
+from .types import *
+from .avatar import Avatar
+from .bonk_maps import OwnMap, Bonk1Map, Bonk2Map
+from .friend_list import FriendList, Friend, FriendRequest, LegacyFriend
+from .game import (
+    Game,
+    Player,
+    Message,
+    MapRequestHost,
+    MapRequestClient,
+    FriendRequest,
+    Match,
+    PlayerMove,
+    GameConnectionError
+)
+from .room import Room

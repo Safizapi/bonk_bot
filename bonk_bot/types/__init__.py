05038d54c33994fd12cde12dd4730edb74f54903
Safizapi <147392459+Safizapi@users.noreply.github.com>
2024-08-02 00:48:10 +0300

    Create __init__.py

---
+++ bonk_bot/types/__init__.py
@@ -0,0 +1,4 @@
+from .game_inputs import GameInputs, AnyGameInput, all_game_inputs
+from .modes import Modes, AnyMode, all_modes_list
+from .servers import Servers, AnyServer, all_servers_list
+from .teams import Teams, AnyTeam, all_teams_list
